3d22b107174775fe91b4b108d56221b872f8d410
Sarojan Shrestha <myselfsarojan@gmail.com>
2025-04-29 18:54:41 +0545

    random num generator

--- random-num-generator/index.js
+++ random-num-generator/index.js
@@ -1,3 +1,10 @@
-const maxNum=document.getElementById("maxnum");
-const minNum=document.getElementById("minNum");
+
 const submit=document.getElementById("submitBtn");
+
+submit.onclick = function(){
+    const maxNum=parseInt(document.getElementById("maxNum").value);
+    const minNum=parseInt(document.getElementById("minNum").value);
+    let randomNum = Math.random()
+    randomNum = Math.floor(randomNum*(maxNum-minNum))+minNum;
+    alert(`Ranndom number is ${randomNum}`);
+}
