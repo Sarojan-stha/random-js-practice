c69efb001adf48c144c1488146413280ba156f27
Sarojan Shrestha <myselfsarojan@gmail.com>
2025-04-30 13:09:41 +0545

    Method chaining

--- Exercises/methodChaining/methodChain.js
+++ Exercises/methodChaining/methodChain.js
@@ -1,6 +1,6 @@
 let username = prompt(`Enter you name.`);
 // a program to convert first letter into uppercase, remaining lower case, and eliminate white spaces
-//without method chaining
+/*without method chaining
 username = username.trim();
 let firstLetter = username.charAt(0);
 firstLetter = firstLetter.toUpperCase();
@@ -9,4 +9,11 @@ let remainingLetter = username.slice(1);
 remainingLetter = remainingLetter.toLocaleLowerCase();
 
 username = firstLetter + remainingLetter;
+console.log(username); */
+
+//using method chaining
+username = username.trim().charAt(0).toUpperCase() + username.slice(1).toLowerCase();
 console.log(username);
+console.log(`hello`);
+
+
