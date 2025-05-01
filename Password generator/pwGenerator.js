5fbbffac6f6539699d4fa2130a9036538d15e046
Sarojan Shrestha <myselfsarojan@gmail.com>
2025-05-01 19:00:08 +0545

    with html

--- Password generator/pwGenerator.js
+++ Password generator/pwGenerator.js
@@ -1,32 +1,46 @@
 
-function generatePassword(length,includeUpperCase,includeLowerCase,includeNumbers,includeSymbols){
+
+
+function generatePassword(){
+   
+    let pwLength = document.getElementById("pwLength").value;
+    const includeLowerCase = document.getElementById("includeLowerCase");
+    const includeUpperCase = document.getElementById("includeUpperCase");
+    const includeNumbers = document.getElementById("includeNumbers");
+    const includeSymbols = document.getElementById("includeSymbols");
+    const generatePw = document.getElementById("generatePw");
+    const showPw = document.getElementById("showPw");
+
     const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     const lowerCase = "abcdefghijklmnopqrstuvwxyz"
     const numbers = "1234567890"
     const symbols = "~!@#$%^&*()_+"
+
+    pwLength = parseInt(pwLength);
     
     let allowedChars = "";
     let password = "";
     let randomIndex;
 
-    allowedChars += includeUpperCase?  upperCase:"";
-    allowedChars += includeLowerCase?  lowerCase:"";
-    allowedChars += includeNumbers?  numbers:"";
-    allowedChars += includeSymbols?  symbols:"";
+    allowedChars += includeUpperCase.checked?  upperCase:"";
+    allowedChars += includeLowerCase.checked?  lowerCase:"";
+    allowedChars += includeNumbers.checked?  numbers:"";
+    allowedChars += includeSymbols.checked?  symbols:"";
 
-    for(let i=0;i<length;i++){
-        randomIndex = Math.floor(Math.random()*allowedChars.length)+1;
+    if(pwLength<=0){
+       return alert(`The length of character must be at least 1`);
+
+    }
+    if(allowedChars.length===0){
+       return alert(`please choose at least one type of characters`);        
+    }
+
+    for(let i=0;i<pwLength;i++){
+        randomIndex = Math.floor(Math.random()*allowedChars.length);
         password += allowedChars[randomIndex];
     }
-    console.log(`Generated password is :${password}`);
 
-    
-    
+    showPw.innerHTML = `Generated Password is :`+ password
 }
 
-const length = 5;
-const includeUpperCase = true;
-const includeLowerCase = true;
-const includeNumbers = true;
-const includeSymbols = true;
-generatePassword(length, includeUpperCase, includeLowerCase, includeNumbers, includeSymbols);
+
