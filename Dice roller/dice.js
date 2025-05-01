639414a59b4a2a862de4581e37e8596500c6cd41
Sarojan Shrestha <myselfsarojan@gmail.com>
2025-05-01 14:32:59 +0545

    without css

---
+++ Dice roller/dice.js
@@ -0,0 +1,20 @@
+
+function diceRoller(){
+    const numOfDice = document.getElementById("numOfDice").value;
+    const rollDice = document.getElementById("rollDice");
+    const result = document.getElementById("result");
+    const diceImage = document.getElementById("diceImage");
+    let values = [];
+    let images = [];
+    let randNum;
+    for(let i=0;i<numOfDice;i++){
+        randNum = Math.floor(Math.random()*6) + 1;
+        values.push(randNum);
+       images.push(`<img src="dice images/${randNum}.png" alt = "img">`);
+    }
+    result.innerHTML = `dice :${values.join(",")}`
+    diceImage.innerHTML = images.join(" ");
+
+    console.log(numOfDice);
+
+}
