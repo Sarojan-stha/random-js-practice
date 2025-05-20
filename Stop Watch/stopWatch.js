a0ad195b8531638b3c9b3b0fbc9d04f922b658e0
Sarojan Shrestha <myselfsarojan@gmail.com>
2025-05-20 15:58:22 +0545

    functional Stop watch without css

---
+++ Stop Watch/stopWatch.js
@@ -0,0 +1,51 @@
+let timer = null;
+let startTime = 0;
+let elapsedTime = 0;
+let isRunning = false;
+const display = document.getElementById("stopWatch");
+
+function startTimer(){
+    if(!isRunning){
+         startTime = Date.now() - elapsedTime;
+         timer = setInterval(update,10);
+         isRunning = true;
+    }
+
+}
+
+function stopTimer(){
+    if(isRunning){
+        clearInterval(timer);
+        elapsedTime = Date.now() - startTime;
+        isRunning = false;
+    }
+
+}
+
+function resetTimer(){
+    clearInterval(timer);
+    startTime = 0;
+    elapsedTime = 0;
+    isRunning = false;
+    display.textContent = "00:00:00:00";
+}
+
+
+function update(){
+    currentTime = Date.now();
+    elapsedTime = currentTime - startTime;
+    
+    let hours = Math.floor(elapsedTime/(1000*60*60));
+    let minutes  = Math.floor(elapsedTime/(1000*60) % 60);
+    let seconds = Math.floor(elapsedTime/1000 % 60);
+    let miliseconds = Math.floor(elapsedTime % 1000 /10 );
+
+    hours = hours.toString().padStart(2,"0");
+    minutes = minutes.toString().padStart(2,"0");
+    seconds = seconds.toString().padStart(2,"0");
+    miliseconds = miliseconds.toString().padStart(2,"0");
+
+
+    display.textContent = `${hours}:${minutes}:${seconds}:${miliseconds}`;
+
+}
