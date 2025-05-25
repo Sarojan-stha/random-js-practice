7009284f294c387652e88fc1e93b05ac4d5ff03a
Sarojan Shrestha <myselfsarojan@gmail.com>
2025-05-25 18:37:52 +0545

    Functionality added

--- Calculator/calculator.js
+++ Calculator/calculator.js
@@ -1,3 +1,17 @@
 const display  = document.getElementById("display");
 
-display.value ="Hello";
+function appendToDisplay(input){
+    display.value = display.value + input;
+}
+
+function clearDisplay(){
+    display.value = " ";
+}
+
+function erase(){
+    display.value = display.value.slice(0,-1);
+}
+
+function result(){
+    display.value = eval(display.value);
+}
