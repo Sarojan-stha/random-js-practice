13f4b4c0e9b4ac5135354441ef3082eab3d88648
Sarojan Shrestha <myselfsarojan@gmail.com>
2025-05-28 08:02:00 +0545

    DOM description

---
+++ DOM/dom.js
@@ -0,0 +1,16 @@
+// DOM             :Document object model   
+//                  objects that represent the page you see in the web browwser and provides you 
+//                  with an api to ineract with it.Web bowser constructs an dom when it loads and
+//                  html document,and structres all the elements in tree like represenatation,
+//                  js can access the dom dynamically to change content ,structure and style of a web page
+
+// ELement selector        :methods to target and manipulate html elements,allows you to
+//                          select one or multiple elements from the dom
+
+
+//document.getElementById()         :HTML ELEMENT AND NULL
+//documnet.getElementsByClassName() :HTML COLLECTION
+//document.getElementByTagName()    :HTML COLLECTION
+//document.querySelector()          :FIRST ELEMENT AND NULL
+//document.querySelectorAll()       :NODE LISTS
+
